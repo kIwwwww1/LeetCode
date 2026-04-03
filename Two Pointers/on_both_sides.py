0066b9a2efbc251308c3ef14b702f4d9f3283948
kIwwwww1 <danil29070@gmail.com>
2026-04-03 14:03:42 +0300

    .

---
+++ Two Pointers/on_both_sides.py
@@ -0,0 +1,18 @@
+def two_sum(nums: list[int], target: int) -> list[int]:
+
+    left = 0
+    right = len(nums) - 1
+
+    while left < right:
+        curr_sum = nums[left] + nums[right]
+
+        if curr_sum == target:
+            return [left, right]
+        elif curr_sum > target:
+            right -= 1
+        else:
+            left += 1
+
+    return [-1, -1]
+
+print(two_sum([-2, 1, 6, 9, 12, 21], target=18))
