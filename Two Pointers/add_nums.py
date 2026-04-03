8db5c6426cd8fd18ac5d67200ca7125ea786648c
kIwwwww1 <danil29070@gmail.com>
2026-04-03 14:23:12 +0300

    .

---
+++ Two Pointers/add_nums.py
@@ -0,0 +1,18 @@
+def add_nums(nums1: list[int], nums2: list[int]):
+    l1, l2 = 0, 0
+    result = []
+
+    while l1 < len(nums1) and l2 < len(nums2):
+        if nums1[l1] < nums2[l2]:
+            result.append(nums1[l1])
+            l1 += 1
+        else:
+            result.append(nums2[l2])
+            l2 += 1
+        
+    result.extend(nums1[l1:])
+    result.extend(nums2[l2:])
+
+    return result
+
+print(add_nums([1,3,5,7], [2,4,6,8]))
