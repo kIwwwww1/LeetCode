5446291ea33196a17cfc589710e0725459fa7d8f
kIwwwww1 <danil29070@gmail.com>
2026-04-07 15:37:59 +0300

    .

---
+++ Hash Map/get_palindrom.py
@@ -0,0 +1,21 @@
+# Время выполнения: O(n)
+# Память: O(1)
+
+def get_palindrom(st: str) -> bool:
+    count = {}
+
+    for sim in st:
+        count[sim] = count.get(sim, 0) + 1
+
+    odd_count = 0
+
+    for ch in count.values():
+        if ch % 2 == 1:
+            odd_count += 1
+    
+    return odd_count <= 1
+
+print(get_palindrom('ABC'))
+print(get_palindrom('AABBCC'))
+print(get_palindrom('AABBCABC'))
+print(get_palindrom('ABCABCC'))
