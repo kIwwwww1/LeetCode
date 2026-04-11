6dad3e5c144969729a098bcbe9619e8f910d1947
kIwwwww1 <danil29070@gmail.com>
2026-04-11 12:53:15 +0300

    .

---
+++ Stack/stack_intermediate_results.py
@@ -0,0 +1,27 @@
+# Время выполнения:
+# Память: 
+
+def is_valid(s: str) -> bool:
+    mapping = {
+        '{': '}',
+        '(': ')',
+        '[': ']',
+        '<': '>',
+    }
+    stack = []
+
+    for char in s:
+        if char in mapping:
+            stack.append(char)
+        elif len(stack) == 0:
+            return False
+        else:
+            last_char = stack.pop()
+            if mapping[last_char] != char:
+                return False
+
+    return len(stack) == 0
+
+print(is_valid('()<[{}]>'))
+print(is_valid('()<[}]>'))
+print(is_valid(')()<[{}]>'))
