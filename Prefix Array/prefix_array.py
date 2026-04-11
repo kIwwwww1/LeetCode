8311de3f341b5b6033110b02068317158faa9935
kIwwwww1 <danil29070@gmail.com>
2026-04-11 13:45:29 +0300

    .

---
+++ Prefix Array/prefix_array.py
@@ -0,0 +1,14 @@
+class PrefixArray:
+    def __init__(self, nums: list[int]) -> None:
+        self.prefix = [0]
+        for num in nums:
+            self.prefix.append(self.prefix[-1] + num)
+        
+    def sum(self, l: int, r: int) -> int:
+        return self.prefix[r + 1] - self.prefix[l]
+
+pref = PrefixArray([1,4,5,-3,7,2])
+print(pref.sum(1, 5))
+
+
+        
