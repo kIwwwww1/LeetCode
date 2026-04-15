85d95ad09dfedec95c30cd725955971e3715b854
kIwwwww1 <danil29070@gmail.com>
2026-04-15 17:29:15 +0300

    .

---
+++ Binary Tree/count_node.py
@@ -0,0 +1,29 @@
+# Время выполнения: O(n)
+# Память: O(k)
+
+class TreeNode:
+    def  __init__(self, val=0, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+root = TreeNode(5)
+
+root.left = TreeNode(8)
+root.right = TreeNode(1)
+
+root.left.left = TreeNode(4)
+
+root.right.left = TreeNode(4)
+root.right.right = TreeNode(1)
+
+def count_nodes(node: TreeNode) -> int:
+    if node is None: 
+        return 0
+    
+    left = count_nodes(node.left)
+    right = count_nodes(node.right)
+
+    return left + right + 1
+
+print(count_nodes(root))
