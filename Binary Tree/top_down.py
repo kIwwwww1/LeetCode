450dce80b74e4366512c5b5b67ea708745c5b040
kIwwwww1 <danil29070@gmail.com>
2026-04-16 11:54:48 +0300

    .

--- Binary Tree/top_down.py
+++ Binary Tree/top_down.py
@@ -9,12 +9,25 @@ class TreeNode:
 
 root = TreeNode(5)
 
-root.left = TreeNode(2)
-root.left.left = TreeNode(3)
+root.left = TreeNode(3)
+root.left.left = TreeNode(2)
 root.left.right = TreeNode(4)
 
-root.left.left.left = TreeNode(5)
+root.right = TreeNode(1)
 
-root.left.right = TreeNode(4)
-root.left.right.right = TreeNode(6)
+def has_path_sum(root: TreeNode, target_sum: int) -> bool:
+    def dfs(node: TreeNode, curr_sum: int) -> bool:
+        if node is None:
+            return False
+        
+        curr_sum += node.val
+
+        if node.left is None and node.right is None:
+            return curr_sum == target_sum
+        
+        return dfs(node.left, curr_sum) or dfs(node.right, curr_sum)
+    
+    return dfs(root, 0)
+
+print(has_path_sum(root, 12))
 
