9f93e5f184309346c2f55d651782fb4587f625e8
kIwwwww1 <danil29070@gmail.com>
2026-04-16 15:44:14 +0300

    .

--- Graphs/create_directed_graph.py
+++ Graphs/create_directed_graph.py
@@ -6,6 +6,5 @@ def graph_from_edges(edges: list[list[int]]) -> dict[int, list[int]]:
     for s, e in edges:
         graph[s].append(e)
     return graph
-        
 
 print(graph_from_edges([[1,2], [8,1], [3,2], [1,3]]))
--- Graphs/create_undirected_graph.py
+++ Graphs/create_undirected_graph.py
@@ -3,9 +3,9 @@ from collections import defaultdict
 # Неориентированный
 def graph_from_edges(edges: list[list[int]]) -> dict[int, list[int]]:
     graph = defaultdict(list)
-    for u, v in edges:
-        graph[u].append(v)
-        graph[v].append(u)
+    for s, e in edges:
+        graph[s].append(e)
+        graph[e].append(s)
     return graph
 
 print(graph_from_edges([[1,2], [8,1], [3,2], [1,3]]))
